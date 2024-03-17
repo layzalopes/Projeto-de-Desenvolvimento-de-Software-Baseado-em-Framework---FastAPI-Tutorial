19aae2c79e8a39b8af119477519800e4c5f12bb1
Láyza Lopes <112975441+lyzlopess@users.noreply.github.com>
2024-03-17 20:47:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Rota inicial
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+async def read_root():
+    return {"message": "Bem-vindo à API de marcas de paçoca!"}
